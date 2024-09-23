3eb3b2ecf1b7be3ce4e6e633750ad33ba3f7d5db
Igor Anić <igor.anic@gmail.com>
2024-09-23 13:26:54 +0200

    add export dashboards script

---
+++ export_dashboards.sh
@@ -0,0 +1,22 @@
+# podman run --network grafana-graphite-statsd_default \
+#     -v ./grafana/dashboards:/dashboards \
+#     grafana-dashboard-manager \
+#     download \
+#     --scheme http \
+#     --port 3000 \
+#     --host grafana \
+#     --non-interactive \
+#     --username admin --password admin \
+#     --destination /dashboards
+
+
+
+
+export HOST=http://localhost:3000
+
+for dash in $(curl -s -k -H "Authorization: Bearer ${KEY}" ${HOST}/api/search\?query\=\& | jq -r '.[] | select(.type == "dash-db") | .uid'); do
+  curl -s -k -H "Authorization: Bearer ${KEY}" ${HOST}/api/dashboards/uid/$dash \
+    | jq '. |= (.folderUid=.meta.folderUid) | del(.meta) |del(.dashboard.id) + {overwrite: true}' \
+    > "./grafana/dashboards/${dash}.json"
+  echo "Dashboard: ${NAME} - ${dash} saved."
+done
